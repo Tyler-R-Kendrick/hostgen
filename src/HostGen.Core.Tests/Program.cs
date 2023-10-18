c33fc95db87a5e755ae424c9e881fd2a100e0811
Tyler-R-Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2023-10-18 23:45:56 +0000

    initial generator checkin

---
+++ src/HostGen.Core.Tests/Program.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp;
+
+partial class Program
+{
+    //[ModuleInitializer]
+    static void Main(string[] args)
+    {
+        HelloFrom("Generated Code");
+    }
+
+    static partial void HelloFrom(string name);
+}
